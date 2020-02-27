66ff5385794ed57f765905f2f256f63b19d0768f
whichstar <821296576@qq.com>
2020-02-27 16:33:05 +0800

    commit first

---
+++ app/src/main/webapp/precache-manifest.9b1b49d354b5ffe9aeaf7a2af7ef48ca.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfdbf5b0d01b283df156",
+    "url": "static/js/chunk-vendors.f1d0708c.js"
+  },
+  {
+    "revision": "335d3d147ec53a823278",
+    "url": "static/css/app.081c2cc2.css"
+  },
+  {
+    "revision": "760eeb475f4b77f91b63",
+    "url": "static/css/chunk-1c90cf17.4a76f2d0.css"
+  },
+  {
+    "revision": "760eeb475f4b77f91b63",
+    "url": "static/js/chunk-1c90cf17.7223258d.js"
+  },
+  {
+    "revision": "bfd29dc4a4f32a5446ed",
+    "url": "static/css/chunk-2c9c92d9.9d153613.css"
+  },
+  {
+    "revision": "bfd29dc4a4f32a5446ed",
+    "url": "static/js/chunk-2c9c92d9.9eab99b1.js"
+  },
+  {
+    "revision": "af34cd445a4fd05075fe",
+    "url": "static/js/chunk-2d0d63f1.c7d5044d.js"
+  },
+  {
+    "revision": "bfdbf5b0d01b283df156",
+    "url": "static/css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "335d3d147ec53a823278",
+    "url": "static/js/app.71913407.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "static/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "71026f375daa4b278f6ade2ffbe469eb",
+    "url": "static/fonts/iconfont.71026f37.eot"
+  },
+  {
+    "revision": "8492342027e2dab448476eb69d466a7f",
+    "url": "static/fonts/iconfont.84923420.ttf"
+  },
+  {
+    "revision": "1d20ca9070f6a3436b435c4919b19401",
+    "url": "static/img/iconfont.1d20ca90.svg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "f4b99a39df9fc18dcf7bad27ac9bf6e4",
+    "url": "index.html"
+  }
+];
