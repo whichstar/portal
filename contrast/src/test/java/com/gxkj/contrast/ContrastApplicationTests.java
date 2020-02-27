014b92ff640bdae1a154b18c8b7ce6da1c5ef33f
whichstar <821296576@qq.com>
2020-02-27 13:47:14 +0800

    commit first

---
+++ contrast/src/test/java/com/gxkj/contrast/ContrastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxkj.contrast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContrastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ interfaces/src/test/java/com/gxkj/interfaces/InterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxkj.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
